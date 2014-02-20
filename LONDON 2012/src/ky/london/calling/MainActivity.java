0c53686ffed3923d3e5dd569742ef7d94ac769b3
Kay <cynosureky@gmail.com>
2014-02-20 11:35:55 +0400

    London2012

---
+++ LONDON 2012/src/ky/london/calling/MainActivity.java
@@ -0,0 +1,53 @@
+package ky.london.calling;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+
+public class MainActivity extends Activity {
+
+    HTML5WebView mWebView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mWebView = new HTML5WebView(this);
+
+        if (savedInstanceState != null) {
+            mWebView.restoreState(savedInstanceState);
+        } else {    
+            mWebView.loadUrl("http://bbcsportintl-f.akamaihd.net/sport_stream_6_hi@35140?__gda__=1341758785_e8087a22abf8e4fdf5d5612f2fcc8b94");
+            mWebView.getSettings().setPluginsEnabled(true);
+        }
+       
+
+        setContentView(mWebView.getLayout());
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mWebView.saveState(outState);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mWebView.stopLoading();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (mWebView.inCustomView()) {
+                mWebView.hideCustomView();
+            //  mWebView.goBack();
+                //mWebView.goBack();
+                return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
